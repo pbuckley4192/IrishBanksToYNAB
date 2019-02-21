68bbd3e74c9e489a25a028c6b5cb0b3ee3bb93e5
Paul <pbuckley3@hotmail.com>
2019-02-21 15:15:34 +0000

    Update digest.py

--- digest.py
+++ digest.py
@@ -1,10 +1,8 @@
 import csv
 import sqlite3
 
-
-
 # Open CSV File
-file = open('statment.csv', mode='r')
+file = open('statement.csv', mode='r')
 csv_file = csv.DictReader(file)
 
 # Open DB
@@ -16,23 +14,19 @@ c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS transactions
              (date text, type text,contactless bool, trans text, change real, balance real)''')
 
-
+# Parse out the statment
 for row in csv_file:
 	descript=""
 	contact="False"
 	if str(row[" Type"]).startswith("POS"):
-	 	#print(row[" Description"])
 		subs = str(row[" Description"]).split(",", 1)
 		descript = subs[1].lstrip()
 		contact =str(str(subs[0]).endswith("C "))
 		
 	else:
-		#print(row[" Description"])
-		#print(row[" Description"].lstrip() +"\n")
 		descript = row[" Description"].lstrip()
 
 	# Insert a row of data
-	#print(("INSERT INTO transactions VALUES ("+row['Date']+",\""+ row[' Type']+"\",\"false\",\""+ descript +"\","+ row[' Value'] +","+ row[' Balance'] + ")"))
 	c.execute("INSERT INTO transactions VALUES (\""+row['Date']+"\",\""+ row[' Type']+"\",\""+contact+"\",\""+ descript +"\","+ row[' Value'] +","+ row[' Balance'] + ")")
 
 # Save (commit) the changes
